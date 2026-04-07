82adc1ce0f55b995aa7d144834eae1317e28fd82
JoJo <joseph.tonski@gmail.com>
2026-04-07 15:52:32 +0000

    feat: Add features, extension, and postCreate script

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,8 @@
+touch .devcontainer/postCreate.sh
+
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install sl
+echo "export PATH=\$PATH:/usr/games" >> ~/.bashrc
+echo "export PATH=\$PATH:/usr/games" >> ~/.zshrc
